97c2c06dd92578df11be8f9f7894c8f35c8b5b7f
jtwray <tuckerwray84@gmail.com>
2019-06-23 14:48:50 -0400

    Initialized Avenegers app to work with React LifeCycle Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable no-tabs ,use 'esversion: 6' */
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			chars: ['Luke', 'Leia', 'Han', 'Chewy', 'C3-P0']
+		};
+	}
+
+	render() {
+		const { chars } = this.state;
+		return (
+  <div>
+    <h2>May The Force be with You!</h2>
+    {chars.map((char, index) => [
+      <div key={char + index}>
+        <p>Name:</p>
+        {char}
+      </div>
+					])}
+  </div>
+		);
+	}
 }
 
 export default App;
